fda966c388c37bb73b822887f81f417c01c7d3f1
renataferrando <renataferrando@hotmail.com>
2021-09-22 14:39:41 -0300

    navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+//NAVBAR
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () =>{
+	const currentScroll = window.pageYOffset
+	if (currentScroll <= 0) {
+		body.classList.remove("scroll-up")
+	}
+	if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+		body.classList.remove("scroll-up")
+		body.classList.add("scroll-down")
+	}
+	if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+		body.classList.remove("scroll-down")
+		body.classList.add("scroll-up")
+	}
+	lastScroll =  currentScroll;  
+})
+
+
+//buton
 $('.button--bubble').each(function() {
     var $circlesTopLeft = $(this).parent().find('.circle.top-left');
     var $circlesBottomRight = $(this).parent().find('.circle.bottom-right');
